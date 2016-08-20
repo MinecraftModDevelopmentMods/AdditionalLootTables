b3a35592d5fba7bdaacadee87a90eeb9b1cabbdd
Jasmine Iwanek <jasmine@iwanek.co.uk>
2016-08-20 16:37:36 -0700

    clean build.gradle, add automated curse upload support.

--- src/main/java/cyano/alt/AdditionalLootTables.java
+++ src/main/java/cyano/alt/AdditionalLootTables.java
@@ -63,7 +63,7 @@ public class AdditionalLootTables
 {
 	public static final String NAME = "Additional Loot Tables";
 	public static final String MODID = "alt";
-	public static final String VERSION = "1.06";
+	public static final String VERSION = "1.1.0";
 
 	public static boolean enabled = true;
 	public static boolean strict_mode = false;
