68342606c0fa93b71b17072c2def9ecd009a5e53
Daniel Hazelton <dshadowwolf@gmail.com>
2017-06-27 04:18:00 -0500

    update the version number and make damned sure that gradle.properties is in the repo - some fucker had added it to .gitignore and it got killed during the defenstration fallout cleanup

--- src/main/java/com/mcmoddev/alt/AdditionalLootTables.java
+++ src/main/java/com/mcmoddev/alt/AdditionalLootTables.java
@@ -29,7 +29,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class AdditionalLootTables {
 	public static final String NAME = "Additional Loot Tables";
 	public static final String MODID = "alt";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "2.0.0";
 
 	@Instance
 	public static AdditionalLootTables INSTANCE = null;
