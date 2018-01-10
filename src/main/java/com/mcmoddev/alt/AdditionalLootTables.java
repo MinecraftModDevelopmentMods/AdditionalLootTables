0dc6f3832a27274f26af5bd3d1c6bbc0ed9b2e44
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-01-09 16:02:01 -0800

    Jar signing and updates

--- src/main/java/com/mcmoddev/alt/AdditionalLootTables.java
+++ src/main/java/com/mcmoddev/alt/AdditionalLootTables.java
@@ -1,7 +1,5 @@
 package com.mcmoddev.alt;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -15,41 +13,49 @@ import com.mcmoddev.alt.api.PluginLoader;
 import com.mcmoddev.alt.commands.ALTDumpCommand;
 
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent; 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = AdditionalLootTables.MODID, version = AdditionalLootTables.VERSION, name= AdditionalLootTables.NAME)
+@Mod(modid = AdditionalLootTables.MODID,
+	version = AdditionalLootTables.VERSION,
+	name = AdditionalLootTables.NAME,
+	acceptedMinecraftVersions = "[1.11.2,)",
+	certificateFingerprint = "@FINGERPRINT@")
 public class AdditionalLootTables {
 	public static final String NAME = "Additional Loot Tables";
 	public static final String MODID = "alt";
-	public static final String VERSION = "2.0.2";
+	public static final String VERSION = "2.0.3";
 
 	@Instance
 	public static AdditionalLootTables INSTANCE = null;
 	public static Config config = null;
 	public static Logger logger = LogManager.getFormatterLogger(MODID);
-	public static final String ALTFolderName = "additional-loot-tables";
-	public static String ALTBaseConfigPath = null;
+	public static final String ALT_FOLDER_NAME = "additional-loot-tables";
 	private static Path loot_folder;
     private static final String PROXY_BASE = "com.mcmoddev." + MODID + ".proxy.";
     private static final PluginLoader pluginLoader = new PluginLoader();
     
     @SidedProxy(clientSide = PROXY_BASE + "ClientProxy", serverSide = PROXY_BASE + "ServerProxy")
     public static CommonProxy proxy;
-	
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+		logger.warn("Invalid fingerprint detected!");
+	}
+
 	@EventHandler
 	public static void preInit(FMLPreInitializationEvent event) {
-		ALTBaseConfigPath = event.getSuggestedConfigurationFile().getParent();
-		loot_folder = Paths.get(ALTBaseConfigPath,ALTFolderName);
+		final String ALT_BASE_CONFIG_PATH = event.getSuggestedConfigurationFile().getParent();
+		loot_folder = Paths.get(ALT_BASE_CONFIG_PATH, ALT_FOLDER_NAME);
 		config = new Config(event);
 		ALTFileUtils.createDirectoryIfNotPresent(loot_folder);
 		pluginLoader.load(event);
